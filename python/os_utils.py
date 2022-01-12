b78fd788c3057c2f0506e6a3cf881e35911ccd14
Ali Babolhavaeji <ababolha@cccis.com>
2022-01-12 16:35:39 -0600

    Pandas datafram splitter function is added to python utils.
    Check and create directories is added to os_utils.

--- python/os_utils.py
+++ python/os_utils.py
@@ -3,8 +3,16 @@ import os
 # Create a new directory if it'sn't exist
 
 def makeDirs_if_not_exists(dirPth ,  verbose = False):
-    if not os.path.exists(dirPth):
+    # if not os.path.exists(dirPth):  # also you can use os.path.isdir() instead
+    #     os.makedirs(dirPth)
+    #     if verbose:
+    #         print(dirPth ,"  is created.")
+
+    dirPth = dirPth.strip(' ')
+    try:
         os.makedirs(dirPth)
         if verbose:
             print(dirPth ,"  is created.")
+    except:
+        return False
 
