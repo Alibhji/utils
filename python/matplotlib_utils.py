1ea77d2b34e7dd4c36162156f6693350b27991e5
Ali Babolhavaeji <ababolha@cccis.com>
2022-01-27 11:48:11 -0600

    cv2_utils added. [overlay two images] matplotlib utils added. [plot a list of images in different rows]

--- python/matplotlib_utils.py
+++ python/matplotlib_utils.py
@@ -2,6 +2,8 @@
 # this file contains snippet scripts for Matplotlib
 
 import matplotlib.pyplot as plt
+import numpy as np
+import cv2
 
 # Display a list of images in a row
 # It takes ( [img1,img2, ...] , ['title1' , 'title2',...])
@@ -13,3 +15,35 @@ def display(display_list , title):
         plt.imshow(display_list[i])
         plt.axis('off')
     plt.show()
+
+
+def display_in_different_rows( img_pathes , each_row = 4 , tagnet = None):
+    def read_image(path):
+        img = cv2.imread(path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        return img
+
+    nsegs = len(img_pathes) // each_row
+
+    display_list = [read_image(img) for img in img_pathes]
+    splited_list = np.array_split(display_list, nsegs)
+    nrows = len(splited_list)
+    ncols = len(splited_list[0])
+    count = 0
+    plt.figure(figsize=(15 , 3*nrows))
+    for r ,row in enumerate(splited_list):
+        for i in range(len(row)):
+            count+=1
+            plt.subplot(nrows, ncols ,count )
+            title = f"{img_pathes[count-1].split('/')[-1]}"
+
+            if not tagnet is None:
+                title += f" {tagnet[count-1]}"
+            plt.title(title)
+
+            plt.imshow(row[i])
+            plt.axis('off')
+    plt.subplots_adjust(wspace=0, hspace=0.12)
+    plt.show()
+
+
